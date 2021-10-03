2d5b3d2f0603bd9761db16fa614db32e75962b75
root <webam@outlook.com>
2021-10-03 19:00:38 +0000

    committing api check

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-    const response = new Response("Hello World!", {
+    const response = new Response("Hello World! Check", {
       headers: { "content-type": "text/plain" },
     });
     event.respondWith(response);
